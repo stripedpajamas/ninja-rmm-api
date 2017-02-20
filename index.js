3d0a4870a5c1fc7f24f6f461f9be9c2d86c18ef4
peter squicciarini <peter.j.squicciarini@gmail.com>
2017-02-19 21:40:59 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+/**
+ * Created by petersquicciarini on 2/18/17.
+ */
+
+var crypto = require('crypto');
+var url = require('url');
+
+
+module.exports = function(apiDetails) {
+
+    function buildSendString(method, contentMd5, contentType, date, resource) {
+        var sendString = '';
+        sendString += method + '\n';
+        sendString += contentMd5 ? contentMd5 : '\n';
+        sendString += contentType ? contentType : '\n';
+        sendString += date + '\n';
+        sendString += resource;
+        return Buffer.from(sendString).toString('base64');
+    }
+
+    return {
+        generateOptions: function(reqObj) {
+
+            if (!['alerts', 'customers', 'devices'].includes(reqObj.resource.split('/')[0])) {
+                return 'Invalid Resource';
+            }
+
+            reqObj.resource = apiDetails.ver + reqObj.resource;
+            reqObj.date = new Date().toUTCString();
+
+            var sendString = buildSendString(reqObj.method, reqObj.contentMd5 || null, reqObj.contentType || null, reqObj.date, reqObj.resource);
+            var signature = crypto.createHmac('sha1', apiDetails.secret).update(sendString).digest('base64');
+
+            return {
+                url: url.resolve(apiDetails.host, reqObj.resource),
+                method: reqObj.method,
+                headers: {
+                    'x-nj-date': reqObj.date,
+                    'Authorization': `NJ ${apiDetails.accessKeyID}:${signature}`
+                }
+            };
+        }
+    };
+
+};
