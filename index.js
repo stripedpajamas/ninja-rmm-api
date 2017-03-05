e331005a03fce1b98583c6ed149bca5cbcf314b2
peter squicciarini <peter.j.squicciarini@gmail.com>
2017-03-05 12:54:14 -0500

    switched variable declarations to ES6

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 /**
- * Created by petersquicciarini on 2/18/17.
+ * Created by peter squicciarini on 2/18/17.
  */
-
-var crypto = require('crypto');
-var url = require('url');
+const crypto = require('crypto');
+const url = require('url');
 
 
 module.exports = function(apiDetails) {
 
     function buildSendString(method, contentMd5, contentType, date, resource) {
-        var sendString = '';
+        let sendString = '';
         sendString += method + '\n';
         sendString += contentMd5 ? contentMd5 : '\n';
         sendString += contentType ? contentType : '\n';
@@ -23,8 +22,8 @@ module.exports = function(apiDetails) {
 
             reqObj.date = new Date().toUTCString();
 
-            var sendString = buildSendString(reqObj.method, reqObj.contentMd5 || null, reqObj.contentType || null, reqObj.date, reqObj.resource);
-            var signature = crypto.createHmac('sha1', apiDetails.secret).update(sendString).digest('base64');
+            let sendString = buildSendString(reqObj.method, reqObj.contentMd5 || null, reqObj.contentType || null, reqObj.date, reqObj.resource);
+            let signature = crypto.createHmac('sha1', apiDetails.secret).update(sendString).digest('base64');
 
             return {
                 url: url.resolve(apiDetails.host, reqObj.resource),
@@ -36,5 +35,4 @@ module.exports = function(apiDetails) {
             };
         }
     };
-
 };
